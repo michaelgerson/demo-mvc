822ad4cec74cd4d91ec37545e493ff05fbcd9881
Michael <michaelgerson@gmail.com>
2021-01-13 13:18:50 -0400

    Aula 53 - Buscar Funcionario por Nome

--- src/main/java/com/mballem/curso/boot/dao/FuncionarioDao.java
+++ src/main/java/com/mballem/curso/boot/dao/FuncionarioDao.java
@@ -1,12 +1,13 @@
 package com.mballem.curso.boot.dao;
 
-import java.util.List;
-
 import com.mballem.curso.boot.domain.Funcionario;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FuncionarioDao {
-    
-	void save(Funcionario funcionario);
+
+    void save(Funcionario funcionario);
 
     void update(Funcionario funcionario);
 
@@ -15,4 +16,14 @@ public interface FuncionarioDao {
     Funcionario findById(Long id);
 
     List<Funcionario> findAll();
+
+    List<Funcionario> findByNome(String nome);
+
+    List<Funcionario> findByCargoId(Long id);
+
+    List<Funcionario> findByDataEntradaDataSaida(LocalDate entrada, LocalDate saida);
+
+    List<Funcionario> findByDataEntrada(LocalDate entrada);
+
+    List<Funcionario> findByDataSaida(LocalDate saida);
 }
--- src/main/java/com/mballem/curso/boot/service/FuncionarioServiceImpl.java
+++ src/main/java/com/mballem/curso/boot/service/FuncionarioServiceImpl.java
@@ -1,13 +1,14 @@
 package com.mballem.curso.boot.service;
 
-import java.util.List;
-
+import com.mballem.curso.boot.dao.FuncionarioDao;
+import com.mballem.curso.boot.domain.Funcionario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.mballem.curso.boot.dao.FuncionarioDao;
-import com.mballem.curso.boot.domain.Funcionario;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -36,14 +37,39 @@ public class FuncionarioServiceImpl implements FuncionarioService {
 
 	@Override
 	public Funcionario buscarPorId(Long id) {
-		
+
 		return dao.findById(id);
 	}
 
 	@Override
 	public List<Funcionario> buscarTodos() {
-		
+
 		return dao.findAll();
+	}
+
+	@Override
+	public List<Funcionario> buscarPorNome(String nome) {
+
+		return dao.findByNome(nome);
+	}
+
+	@Override
+	public List<Funcionario> buscarPorCargo(Long id) {
+
+		return dao.findByCargoId(id);
+	}
+
+	@Override
+	public List<Funcionario> buscarPorDatas(LocalDate entrada, LocalDate saida) {
+		if (entrada != null && saida != null) {
+			return dao.findByDataEntradaDataSaida(entrada, saida);
+		} else if (entrada != null) {
+			return dao.findByDataEntrada(entrada);
+		} else if (saida != null) {
+			return dao.findByDataSaida(saida);
+		} else {
+			return new ArrayList<>();
+		}
 	}
 
 }
