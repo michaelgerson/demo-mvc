e048b12055ebe26841e764f24e455849149a7d45
Michael <michaelgerson@gmail.com>
2021-01-13 12:52:51 -0400

    Aula 49 - Inserindo Funcionario

--- src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -1,20 +1,56 @@
 package com.mballem.curso.boot.web.controller;
 
+import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.domain.Funcionario;
+import com.mballem.curso.boot.domain.UF;
+import com.mballem.curso.boot.service.CargoService;
+import com.mballem.curso.boot.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+	@Autowired
+	private CargoService cargoService;
+
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
-	public String listar() {
-		return "/funcionario/lista"; 
+	public String listar(ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
+		return "/funcionario/lista";
+	}
+
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();
+	}
+
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
 	}
 }
