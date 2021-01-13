74b7af8a5f3376917c170875aadbc7a2715024c9
Michael <michaelgerson@gmail.com>
2021-01-13 11:45:35 -0400

    43 - Inserindo cargos

--- src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
@@ -1,20 +1,49 @@
 package com.mballem.curso.boot.web.controller;
 
+import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.domain.Departamento;
+import com.mballem.curso.boot.service.CargoService;
+import com.mballem.curso.boot.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
-	@GetMapping("/cadastrar")
-	public String cadastrar() {
-		return "/cargo/cadastro";
-	}
-	
-	@GetMapping("/listar")
-	public String listar() {
-		return "/cargo/lista"; 
+    @Autowired
+    private CargoService service;
+
+    @Autowired
+    private DepartamentoService departamentoService;
+
+    @GetMapping("/cadastrar")
+    public String cadastrar(Cargo cargo) {
+        return "/cargo/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar(ModelMap model) {
+        return "/cargo/lista";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Cargo cargo, RedirectAttributes attr) {
+        service.salvar(cargo);
+        attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+        return "redirect:/cargos/cadastrar";
+    }
+
+    @ModelAttribute("departamentos")
+	public List<Departamento> listaDepartamentos() {
+    	return departamentoService.buscarTodos();
 	}
 }
